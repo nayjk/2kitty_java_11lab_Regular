36458468cece6438f0e3aac25a7380eb85c73f55
Nayjk <98636064+LobkovaLiza@users.noreply.github.com>
2022-05-27 12:15:25 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -7,7 +7,7 @@ public class Main {
         System.out.println("Напишите ваш айпи адрес"); //000.000.000.000
         Scanner in = new Scanner(System.in);
         String ip = in.next();
-        Pattern pattern = Pattern.compile("\\b([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\.([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\.([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\.([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\b");
+        Pattern pattern = Pattern.compile("\\b\\d\\d\\d\\.\\d\\d\\d\\.\\d\\d\\d\\.\\d\\d\\d");
         Matcher a = pattern.matcher(ip);
         if(a.find()){
             System.out.println("Отлично, теперь я приду к тебе домой ночью по адресу " + a.group());
