5dde1696377ce1330ee1b21ab36e5545c2da0ff6
Nayjk <98636064+LobkovaLiza@users.noreply.github.com>
2022-05-30 08:50:57 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,28 +1,21 @@
+package com.company;
 import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Напишите ваш айпи адрес"); //000.000.000.000
         Scanner in = new Scanner(System.in);
-        String ip = in.next();
-        Pattern pattern = Pattern.compile("\\b\\d\\d\\d\\.\\d\\d\\d\\.\\d\\d\\d\\.\\d\\d\\d");
-        Pattern patterna = Pattern.compile("\\b\\d\\d\\d\\.\\d\\d\\d\\.\\d\\.\\d\\d");
-        Pattern patternс = Pattern.compile("\\b\\d\\d\\d\\.\\d\\d\\d\\.\\d\\d\\.\\d\\d");
-        Matcher a = pattern.matcher(ip);
-        Matcher b = patterna.matcher(ip);
-        if (a.find() && b.find()) {
-            System.out.println("Отлично, теперь я приду к тебе домой ночью по адресу " + a.group());
-            System.out.println("Отлично, теперь я приду к тебе домой ночью по адресу " + b.group());
-        } else {
-            System.out.println("Введите по примеру ");
+        System.out.println("Введите IP адрес: ");
+        String ip = in.nextLine();
+        Pattern pattern = Pattern.compile("\\b([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\.([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\.([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\.([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\b");
+        Matcher m = pattern.matcher(ip);
+        if(m.find()){
+            System.out.println("Верный IP адрес: ");
+            System.out.println(m.group());
         }
-        Matcher d = patternс.matcher(ip);
-        if (d.find()) {
-            System.out.println("Отлично, теперь я приду к тебе домой ночью по адресу " + d.group());
-        } else {
-            System.out.println("Введите по примеру ");
+        else{
+            System.out.println("Не найден верный IP адрес");
         }
     }
 }
